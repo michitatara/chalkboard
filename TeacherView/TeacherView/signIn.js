21bdc7105a6db84667a5d8ee9a769c5a1cd500f0
somezzedup <83977892+somezzedup@users.noreply.github.com>
2021-11-02 18:36:24 -0400

    Add files via upload

--- TeacherView/TeacherView/signIn.js
+++ TeacherView/TeacherView/signIn.js
@@ -4,8 +4,8 @@ $(function(){
         var value=$("input[name=usertype]:checked").val();
         switch(value){
             case "admin":  location.href='AdminHomePage.html'; break;
-            case "hod":  location.href='TeacherHomepage.html.html'; break;
-            case "tstaff":  location.href='StudentHomePage.html'; break;
+            case "teacher":  location.href='TeacherHomepage.html'; break;
+            case "student":  location.href='StudentHomePage.html'; break;
 
         }
     });
