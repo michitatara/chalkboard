65ccbf142c4f0713ffe59ce13fface62ab1b223f
michitatara <torterra10@gmail.com>
2021-12-01 15:18:05 -0500

    json files

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
 const ejs = require('ejs');
 var app = express ();
+app.use('/', express.static(path.join(_dirname, 'static')))
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.listen(3306);
+const path = require('path');
+const PORT = process.env.PORT || 3306;
+app.use(express.static('src'));
+app.listen(3306, () => {
+    console.log(`App listening on port ${PORT}`);
+})
 
 app.get('/', function (req,res){
     res.render('src/index');
-})
+});
