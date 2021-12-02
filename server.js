8cd2977f0e5dd4fdc605b0f5a92170b6e93022ec
michitatara <torterra10@gmail.com>
2021-12-02 18:25:30 -0500

    fix2

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 const express = require('express')
 const path=require('path')
-const bodyParser = require('body-parser')
 const mongoose=require('mongoose')
 const User= require('./models/user')
-
-
+const ejs = require('ejs')
+const port = 3000
+const app = express()
 
 mongoose.connect('mongodb://localhost:27017/login-app-db', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 })
 
-const app = express ()
-app.use('/', express.static(path.join(__dirname, 'static')))
-app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.render('signup');
+});
 
 app.post('/api/register', async(req, res)=> {
    console.log(req.body)
     res.json({status:'ok'})
 })
-app.listen(9999, () =>{
-    console.log('Server up at 9999')
-   
-})
 
-//updated server
+app.listen(port, () =>{
+    console.log(`Server up at ${port}`) 
+})
