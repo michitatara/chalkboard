520e9c0477f97823753119dfcd4afa2302874e63
somezzedup <83977892+somezzedup@users.noreply.github.com>
2021-12-21 13:16:36 -0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,42 +1,16 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema(
-<<<<<<< HEAD
+const userSchema = new mongoose.Schema(
   {
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
+    rid:{ type: String, required: true, unique: true },
     role: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-  },
-  { collection: "users" }
+  }
 );
 
-const model = mongoose.model("UserSchema", UserSchema);
-
-// module.exports = model
-
-module.exports = {
-  fetchData: function (callback) {
-    var userData = model.find({});
-    userData.exec(function (err, data) {
-      if (err) throw err;
-      return callback(data);
-    });
-  },
-};
-=======
-	{
-        firstname: { type: String, required: true},
-        lastname: { type: String, required: true},
-		role: { type: String, required: true},
-		email: { type: String, required: true, unique: true },
-		password: { type: String, required: true }
-	},
-	{ collection: 'users' }
-)
-
-const model = mongoose.model('UserSchema', UserSchema)
+const model = mongoose.model('user', userSchema)
 
 module.exports = model
->>>>>>> refs/remotes/origin/main
